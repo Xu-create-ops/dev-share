6a5b177b031057036ab6c0d95c00dc42a0af94a3
Xu-create-ops <cxu498148@gmail.com>
2025-05-30 17:11:20 +0800

    add validation

--- dev-share-api/Services/BaseShareChainHandle.cs
+++ dev-share-api/Services/BaseShareChainHandle.cs
@@ -6,8 +6,24 @@ public abstract class BaseShareChainHandle : IShareChainHandle
 {
     protected abstract Task<HandlerResult> ProcessAsync(ResourceShareContext context);
 
+    protected void Validate(ResourceShareContext context)
+    {
+    }
+
     public Task<HandlerResult> HandleAsync(ResourceShareContext context)
     {
-        return ProcessAsync(context); 
+        try
+        {
+            Validate(context);
+            return ProcessAsync(context); 
+        }
+        catch (ArgumentException ex)
+        {
+            return Task.FromResult(HandlerResult.Fail(ex.Message));
+        }
+        catch (Exception ex)
+        {
+            return Task.FromResult(HandlerResult.Fail(ex.Message));
+        }
     }
 }
